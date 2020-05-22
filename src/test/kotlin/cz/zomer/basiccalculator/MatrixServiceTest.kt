6ee88e149b1873d97c14b50b9452f90eb92a354a
jakub-zomer <jakubzomer@gmail.com>
2020-05-23 00:41:01 +0200

    added the functions of Matrix addition, multiplicaton and subtraction. both matrices are 2 rows by two columns. the var starting with x fill the matrix #1 , starting with y fill matrix #2. The way of printing the result isn`t finalised yet, tests are yet to be done too. so far, it works

---
+++ src/test/kotlin/cz/zomer/basiccalculator/MatrixServiceTest.kt
@@ -0,0 +1,11 @@
+package cz.zomer.basiccalculator
+
+import org.junit.jupiter.api.Test
+
+class MatrixServiceTest
+
+val testServiceMatrix = MatrixService()
+@Test
+fun matrixAddition(){
+    val matxService = matrixAddition()
+}
